af46cd73ec6d7b47565456106e18cd04daa5d1e5
thekidnamedhosea <hosea.georgeson@gmail.com>
2021-12-31 19:55:55 -0800

    Add html and js

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const trelloBoardController = (function () {})();
+
+const viewController = (function () {})();
+
+const controller = (function () {
+  return {
+    init: function () {
+      console.log('Application has started');
+    },
+  };
+})();
+controller.init();
