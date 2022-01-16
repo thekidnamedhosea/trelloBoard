904ab1831cb7dc9f827adcb6ad4539f515f0ac54
jakethecoder95 <95jacob07@gmail.com>
2022-01-15 20:38:53 -0800

    Add a task on add task button click

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 'use strict';
 
-const trelloBoardController = (function () {})();
+const trelloBoardController = {};
 
-const viewController = (function () {})();
+const viewController = {
+  taskButtonEventListener = (event) => {
+    const targetElm = event.target;
+    targetElm.previousElementSibling.append(
+      viewController.createElement(
+        'li',
+        'Perferendis perspiciatis velit amet odio odit blanditiis',
+        'task'
+      )
+    );
+  },
 
-const controller = (function () {
-  return {
-    init: function () {
-      console.log('Application has started');
-    },
-  };
-})();
+  createElement(type, innerHtml, className) {
+    const newElement = document.createElement(type);
+    newElement.innerHTML = innerHtml;
+    newElement.classList.add(className);
+    return newElement;
+  },
+};
+
+const controller = {
+  addTaskListener() {
+    const addTaskButtons = document.querySelectorAll('.add-task-button');
+    for (const element of addTaskButtons) {
+      element.addEventListener('click', viewController.taskButtonEventListener);
+    }
+  },
+
+  init: function () {
+    console.log('Application has started');
+    controller.addTaskListener();
+  },
+};
 controller.init();
